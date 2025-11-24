bef8d8f886a6f9aba5309587c07c78f2340c7953
Marwane <marwane.seffah@etu.univ-amu.fr>
2025-11-24 12:05:48 +0100

    nouveau fichier : vote_alternatif/.nfs00000001feb1638e00000046 	nouveau fichier : vote_alternatif/SAE_vote_alternatif/.qmake.stash 	modifié :         vote_alternatif/SAE_vote_alternatif/SAE_vote_alternatif.pro.user 	nouveau fichier : vote_alternatif/SAE_vote_alternatif/build/Makefile 	nouveau fichier : vote_alternatif/SAE_vote_alternatif/build/SAE_vote_alternatif 	nouveau fichier : vote_alternatif/SAE_vote_alternatif/build/main.o 	modifié :         vote_alternatif/SAE_vote_alternatif/main.cpp 	modifié :         vote_alternatif/entree1.txt 	nouveau fichier : vote_alternatif/entree_maison.txt 	nouveau fichier : vote_alternatif/oracle_maison.txt

--- vote_alternatif/SAE_vote_alternatif/main.cpp
+++ vote_alternatif/SAE_vote_alternatif/main.cpp
@@ -19,7 +19,7 @@ vector<int> litUnChoix (){
         if ((!cin) || (chaine.substr(0,2) != "//")) break;
     }
 
-    string choix;
+    string choix = "";
     vector<int> choix_liste;
     for (unsigned int i=0; i<chaine.size(); ++i) {
         if (chaine[i] >='0' && chaine[i] <= '9') {
@@ -44,13 +44,6 @@ struct candidat {
     bool estHorsCourse;
 };
 
-/*
-bool compare2part (const participant & p1, const participant & p2){
-    return p1.choix <= p2.choix;
-    error
-}
-*/
-
 void affichVectString (const vector<string> & v){
     for (const string & val : v)
         cout << val << '\t';
@@ -74,27 +67,29 @@ void affichVectParticipants (const vector<participant> & vPart){
 
 int supprimerElem (vector<int> & tab, const size_t & pos){
     int temp = tab[pos];
-    for (size_t i=pos; i<tab.size() -1; i++) {
+    for (size_t i=pos; i<tab.size() -1; ++i) {
         tab[i] = tab[i+1];
     }
     tab.resize(tab.size()-1);
     return temp;
 }
 
-bool choixInvalide(vector<candidat> && tabCand, int && choix) {
-    return tabCand[choix].estHorsCourse || choix < 1 || choix >= int(tabCand.size());
+bool choixInvalide(const vector<candidat> & tabCand, const int & choix) {
+    return tabCand[choix -1].estHorsCourse || choix < 1 || (choix > int(tabCand.size()));
 }
 
 
-int main()
-{
+int main() {
     /*
     cout << "Hello World!" << endl;
     return 0;
     */
+
+
     /* On commence par mettre dans une liste les candidats*/
     vector <candidat> vCandidats;
     string ligne;
+
     while (true) {
         ligne = litUneString();
         if (ligne == string (10, '-')) break; // On considère une ligne de 10 '-' comme la fin de la section de definition des candidats
@@ -109,26 +104,19 @@ int main()
     //On lit les datas du clavier, et on les stocke
     vector<participant> vParticipant;
 
-    for (unsigned i (0); i < 10; ++i){
+    for (unsigned i (0); i < 100; ++i){ // on considère qu'on aura toujours 110 participants
+    //while (true) { // tant qu'on atteint pas la fin du fichier
         string nom (litUneString());
         string prenom  (litUneString());
         vector<int> choixDuParticipant (litUnChoix()); // choix par ordre de préférence décroissante
-
-        //cout << nom << endl << prenom << endl << choixDuParticipant << endl;
-
+        //if (nom == "") break;
         vParticipant.push_back(participant{nom, prenom, choixDuParticipant});
     }
 
-    /* Tri des participants par ordre de choix // Pas obligé ce sera compliqué
-    affichVectParticipants(vParticipant); // liste des participants
-    cout << string (15, '-') << endl;
-    sort (vParticipant.begin(), vParticipant.end(), compare2part); // même liste mais triée en place cette fois
-    affichVectParticipants(vParticipant);
-    */
-
     candidat gagnant;
     bool gagnantTrouve = false;
-    while (gagnantTrouve) {
+    while (!gagnantTrouve) {
+        //cout << gagnantTrouve;
         /*
         On va implémenter ici notre algorithme de code pour le système de vote alternatif
         On a détaillé dans le dossier déjà le fonctionnement de ce système de vote,
@@ -140,9 +128,11 @@ int main()
             // pour que le choix soit valide, il doit correspondre au choix préféré du participant
             // parmi les candidats valides
             bool choixExploitable = vParticipant[i].choix.size() > 0;
-            while (choixExploitable && choixInvalide(vCandidats, vParticipant[i].choix)) {
+            while (choixExploitable && choixInvalide(vCandidats, vParticipant[i].choix[0])) {
                 // on purge au fur et à mesure les candidats hors course des votes des participants
-                supprimerElem(vParticipant[i].choix, 0);
+                if (choixInvalide(vCandidats, vParticipant[i].choix[0])) {
+                    supprimerElem(vParticipant[i].choix, 0);
+                }
                 // on vérifie à chaque fois qu'il y a encore de quoi determiner le vote du participant
                 choixExploitable = vParticipant[i].choix.size() > 0;
             }
@@ -150,7 +140,7 @@ int main()
             // on ajoute aux electeurs du candidat le participant qui lui accorde son vote
             // les numeros de choix des particpants correspondont à leur indice dans la liste
             // vCandidats auquel on additionne 1. On utilise donc -1 pour obtenir l'indice.
-            vCandidats[vParticipant[i].choix[0] -1].electeur.push_pack(vParticipants[i]);
+            vCandidats[vParticipant[i].choix[0] -1].electeurs.push_back(vParticipant[i]);
         }
 
         // on détermine si un des candidats a la majorité absolue
@@ -163,6 +153,7 @@ int main()
                         vCandidats[j].estHorsCourse = true;
                     }
                 }
+                //cout << vCandidats[i].nom;
                 gagnant = vCandidats[i];
                 gagnantTrouve = true;
             }
@@ -175,10 +166,10 @@ int main()
         vector<unsigned int> indicesMin; // on gère les candidats en dernière positions avec une liste pour gérer les ex aequo
         indicesMin.push_back(0);
         for (unsigned int i=1; i<vCandidats.size(); ++i) { // on commence à 1 car on n'a pas besoin de tester pour i=0
-            if (vCandidats[i] == vCandidats[indicesMin[0]]) {
+            if (vCandidats[i].electeurs.size() == vCandidats[indicesMin[0]].electeurs.size()) {
                 indicesMin.push_back(i);
             }
-            else if (vCandidats[i] < vCandidats[indicesMin[0]]) {
+            else if (vCandidats[i].electeurs.size() < vCandidats[indicesMin[0]].electeurs.size()) {
                 vector<unsigned int> temp;
                 temp.push_back(i);
                 indicesMin = temp; // on remplace la liste avec une nouvelle contenant le nouveau minimum
@@ -196,21 +187,21 @@ int main()
             }
         }
         if (indicesMin.size() == nbEnLice) {
-            cout << "Le candidat \"";
+            cout << "Les candidats \"";
             for (unsigned int i=0; i<vCandidats.size(); ++i) {
-                cout << vCandidats[i] << ", ";
+                cout << vCandidats[i].nom << ", ";
             }
-            cout << "ont gagné, félicitation !";
+            cout << "ont gagné: félicitations !" << endl;
             return 0;
         }
-        for (unsigned int i=0; i<indiceMin.size(); ++i) {
-            vCandidats[indiceMin[i]].estHorsCourse = true;
+        for (unsigned int i=0; i<indicesMin.size(); ++i) {
+            vCandidats[indicesMin[i]].estHorsCourse = true;
         }
         // on réinitialise ensuite les electeurs de chaque candidats pour préparer le prochain tour
         for (unsigned int i=0; i<vCandidats.size(); ++i) {
-            vCandidats[i].electeur.resize(0);
+            vCandidats[i].electeurs.resize(0);
         }
     }
-    cout << "Le candidat \"" << vCandidats[0] << "\" a gagné, félicitations !" << endl;
+    cout << "Le candidat \"" << gagnant.nom << "\" a gagné, félicitations !" << endl;
     return 0;
 }
